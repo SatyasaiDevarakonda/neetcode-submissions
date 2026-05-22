32515edfc691a90f8da98b4a2a34ee3ee417ea75
SatyasaiDevarakonda <143203996+SatyasaiDevarakonda@users.noreply.github.com>
2026-05-22 22:16:16 +0530

    Add: concatenation-of-array - submission-0

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.py
@@ -0,0 +1,8 @@
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        n=len(nums)
+        ans= [0]*(2*n)
+        for i in range(len(nums)):
+            num=nums[i]
+            ans[i]= ans[i+n]= num
+        return ans
